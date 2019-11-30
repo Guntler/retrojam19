d70a40e5a5ae81a44782fd27e94fdb466e3ff6a8
MiguelSilveira <migueljasilveira@gmail.com>
2019-11-30 15:23:30 +0000

    Created Stubs for Main Classes

---
+++ RetroJam2019/Assets/Scripts/GameBoard.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System;
+public class GameBoard {
+
+    private int boardXSize = 20;
+    private int boardYSize = 11;
+
+	public Cell[][] board;
+
+	public Cell GetCell(int x, int y) {
+        throw new NotImplementedException("The requested feature is not implemented.");
+    }
+
+    public GameObject[] GetAllItems() {
+        throw new NotImplementedException("The requested feature is not implemented.");
+    }
+
+    public int GetWidth () {
+        return boardXSize;
+    }
+
+    public int GetHeight () {
+        return boardYSize;
+    }
+
+}
+
+public struct Cell{
+    public BoardItemBehavior[] boardItems;
+}
+
