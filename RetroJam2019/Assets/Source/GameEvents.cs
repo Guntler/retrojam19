34f34a11fa3463fcdb8a2bfa4d34d549f06fff44
João Marinheiro <joca.mar2011@gmail.com>
2019-11-30 15:43:39 +0000

    Added game events stubs

---
+++ RetroJam2019/Assets/Source/GameEvents.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Update100Evt : GameEvent
+{
+
+}
+
+public class Update500Evt : GameEvent
+{
+
+}
+
+public class Update1000Evt : GameEvent
+{
+
+}
+
+public class PickedUpDebrisEvt : GameEvent
+{
+
+}
+
+public class RocketCollidedEvt : GameEvent
+{
+
+}
+
+public class TruckCollidedEvt : GameEvent
+{
+
+}
+
+public class DeliveredDebrisEvt : GameEvent
+{
+
+}
+public class AddScoreEvt : GameEvent
+{
+
+}
+public class LaunchedRocketEvt : GameEvent
+{
+
+}
+public class LaunchCountdownEvt : GameEvent
+{
+
+}
+
+public class GameEndEvt : GameEvent
+{
+
+}
+
+public class GameStartEvt : GameEvent
+{
+
+}
+
+public class SpawnRocketEvt : GameEvent
+{
+
+}
