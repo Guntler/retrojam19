807ac54b1a40bd35de0c3d500ed2abb97ee1e043
Guntler <pbpdirector@hotmail.com>
2019-12-01 02:34:29 +0000

    Updated explosion sprite

--- RetroJam2019/Assets/SmallShipExplosionBehavior.cs
+++ RetroJam2019/Assets/SmallShipExplosionBehavior.cs
@@ -14,7 +14,8 @@ public class SmallShipExplosionBehavior : MonoBehaviour
     public void SpawnDebris()
     {
         GameObject debris = Instantiate(DebrisPrefab, transform.position, new Quaternion(0, 0, 0, 0));
-
+        DebrisBehavior debBev = debris.GetComponent<DebrisBehavior>();
+        //GameManager.GetInstance().Board.AddItem(debBev, )
         Destroy(gameObject);
     }
 }
